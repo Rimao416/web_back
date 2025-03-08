8c6b6f96696ef08da96bfbe77d2ccfd7181b7096
Rimao416 <kayumbaomari27@gmail.com>
2025-03-08 19:23:30 +0100

    06_Mongoose

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-const dotenv=require('dotenv')
-const app=require("./index")
-dotenv.config({path:"./config.env"})
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const app = require("./index");
+dotenv.config({ path: "./config.env" });
 // console.log(process.env)
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB)
+  .then(() => console.log("DB connection successful"));
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`App running on port ${port}`);
-  });
+  console.log(`App running on port ${port}`);
+});
