1fea9fe36f42d0c97769d80ab72c45b7e5b80e0f
Rimao416 <kayumbaomari27@gmail.com>
2025-03-08 17:55:12 +0100

    03_Routes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const articles = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/articles.json`)
 );
 app.use(express.json());
-app.get("/api/v1/articles", (req, res) => {
+const getArticles= (req, res) => {
   res.status(200).json({
     status: "sucess",
     length: articles.length,
@@ -13,9 +13,9 @@ app.get("/api/v1/articles", (req, res) => {
       articles,
     },
   });
-});
+};
 
-app.post("/api/v1/articles", (req, res) => {
+const createArticle=(req, res) => {
   // console.log(req.body)
   const newId = articles[articles.length - 1].id + 1;
   const newArticle = Object.assign({ id: newId }, req.body);
@@ -32,8 +32,8 @@ app.post("/api/v1/articles", (req, res) => {
       });
     }
   );
-});
-app.get("/api/v1/articles/:id", (req, res) => {
+};
+const getArticle= (req, res) => {
   const id = req.params.id * 1;
   const articleId = articles.find((el) => el.id === id);
   if (!articleId) {
@@ -49,10 +49,10 @@ app.get("/api/v1/articles/:id", (req, res) => {
       articleId,
     },
   });
-});
+};
 
 
-app.patch("/api/v1/articles/:id", (req, res) => {
+const updateArticle= (req, res) => {
     if (req.params.id *1  >articles.length) {
       return res.status(404).json({
         status: "fail",
@@ -65,9 +65,9 @@ app.patch("/api/v1/articles/:id", (req, res) => {
           article:'<Updated tour here...'
       }
     })
-  });
+  };
   
-  app.delete('/api/v1/articles/:id',(req,res)=>{
+const deleteArticle=(req,res)=>{
       if(req.params.id *1>articles.length){
           return res.status(404).json({
               status:'Fail',
@@ -78,8 +78,16 @@ app.patch("/api/v1/articles/:id", (req, res) => {
           status:"Success",
           data:null
       })
-  })
+  }
 
+//   app.get("/api/v1/articles",getArticles)
+//   app.post("/api/v1/articles",createArticle)
+//   app.get("/api/v1/articles/:id",getArticle),
+//   app.patch("/api/v1/articles/:id",updateArticle)
+//   app.delete("/api/v1/articles/:id",deleteArticle)
+
+app.route("/api/v1/articles").get(getArticles).post(createArticle)
+app.route("/api/v1/articles/:id").get(getArticle).patch(updateArticle).patch(updateArticle).delete(deleteArticle)
 
 const port = 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
